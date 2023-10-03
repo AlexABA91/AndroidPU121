9b07f019a37a8da503341442b89bafeac91e3b67
LimboRider2000 <limboreader@gmail.com>
2023-10-03 16:58:01 +0300

    Initial commit

---
+++ app/src/main/java/com/example/androidpu121/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.androidpu121;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvTitle;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // робота с елиментами UI (Views) - только после setContentView
+        tvTitle = findViewById(R.id.main_tv_title);
+        Button btnViews =  findViewById(R.id.main_btn_views);
+
+        btnViews.setOnClickListener(this::btnViewsClick);
+    }
+    //обработчики событий имеют одинаковый прототип
+     private void btnViewsClick(View view){ // view -sender
+         Intent intent = new Intent(this.getApplicationContext(),
+                 ViewsActivity.class);
+         startActivity(intent);
+     }
+}
